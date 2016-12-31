5fc74945345d77fb1b08ec574e5a5ddd40a94abb
kharland <kendaljharland@gmail.com>
2016-12-31 14:31:26 -0800

    add tool dir

--- bot.js
+++ bot.js
@@ -3,41 +3,41 @@ const fs = require('fs');
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`);
+    console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`);
 });
 
 bot.on('message', msg => {
-	var assets = AudioAssets.load();
-	if (msg.content == '!commands') {
+    var assets = AudioAssets.load();
+    if (msg.content == '!commands') {
         var commands = '!commands !Thanks !ReloadAudio ' +
-			assets.map((a) => a.command).join(' ');
+            assets.map((a) => a.command).join(' ');
         msg.reply(commands);
     } else if (msg.content == '!Thanks') {
         msg.reply("No Problem.");
-	} else if (msg.content == '!ReloadAudio') {
-		AudioAssets.load(true);
+    } else if (msg.content == '!ReloadAudio') {
+        AudioAssets.load(true);
     } else { // Handle audio file commands.
-		assets.forEach(asset => {
-			if (msg.content == asset.command) {
-				playClip(msg,  asset);
-			}
-		});
-	}
+        assets.forEach(asset => {
+            if (msg.content == asset.command) {
+                playClip(msg, asset);
+            }
+        });
+    }
 });
 
 class Asset {
-	constructor(file, volume) {
-		this.file = file;
-		this.volume = volume;
-		this.command = '!' + file.substring(0, file.indexOf('.'));
-		this.path = 'assets/' + file;
-	}
+    constructor(file, volume) {
+        this.file = file;
+        this.volume = volume;
+        this.command = '!' + file.substring(0, file.indexOf('.'));
+        this.path = 'assets/' + file;
+    }
 }
 
 class AudioAssets {
-	// Load all audio assets.
-	//
-	// Will only load files once unless forceReload is true.
+    // Load all audio assets.
+    //
+    // Will only load files once unless forceReload is true.
     static load(forceReload = false) {
         if (AudioAssets._assets == undefined || forceReload) {
             AudioAssets._assets = [];
@@ -46,8 +46,8 @@ class AudioAssets {
                 var asset = new Asset(file, '.30');
                 // adjust for custom volume
                 if (asset.command == '!hugh') {
-                  asset.volume = '2';
-				}
+                    asset.volume = '2';
+                }
                 AudioAssets._assets.push(asset);
                 console.log('loaded asset: ' + asset.path);
             });
@@ -58,19 +58,22 @@ class AudioAssets {
 
 
 function playClip(msg, asset) {
-	console.log(msg.author.username + " used " + msg.content);
+    console.log(msg.author.username + " used " + msg.content);
 
-	var channel = msg.member.voiceChannel;
-	if (channel == undefined){
-		msg.reply("You must be in a voice channel to use this command.");
-	} else {
+    var channel = msg.member.voiceChannel;
+    if (channel == undefined) {
+        msg.reply("You must be in a voice channel to use this command.");
+    } else {
         channel.join().then(connection => {
-        	console.log("playing " + asset.path + " at " + asset.volume);
+            console.log("playing " + asset.path + " at " + asset.volume);
             connection.playFile(
-            	asset.path,
-				{volume: asset.volume})
-				.on('end', () => { channel.leave(); });
-    	});
+                    asset.path, {
+                        volume: asset.volume
+                    })
+                .on('end', () => {
+                    channel.leave();
+                });
+        });
     }
 }
 
